c93d09e667b6b66114b227b1769b8e024d87317f
Seb Avenel <sebastien.avenel@outlook.fr>
2019-10-02 19:11:03 +0200

    get resource

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,25 +12,51 @@ use Symfony\Component\Serializer\SerializerInterface;
 /**
  * Class ProductController
  * @package App\Controller
- * @Route("/api")
+ * @Route("/api/products")
  */
 class ProductController extends Controller
 {
     /**
-     * @Route("/products/{page<\d+>?1}", methods={"GET"})
-     * @param Request $request
-     * @param ProductRepository $repository
-     * @param SerializerInterface $serializer
+     * @var ProductRepository
+     */
+    private $repository;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(ProductRepository $repository, SerializerInterface $serializer)
+    {
+        $this->repository = $repository;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @param Product $product
+     * @Route("/{id}", methods={"GET"})
      * @return Response
      */
-    public function index(Request $request, ProductRepository $repository, SerializerInterface $serializer)
+    public function show(Product $product)
+    {
+        $data = $this->serializer->serialize($product, 'json');
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/{page<\d+>?1}", methods={"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
     {
         $page = $request->query->get('page');
         if(is_null($page) || $page < 1) {
             $page = 1;
         }
-        $products = $repository->findProducts($page, $_ENV['LIMIT']);
-        $data = $serializer->serialize($products, 'json');
+        $products = $this->repository->findProducts($page, $_ENV['LIMIT']);
+        $data = $this->serializer->serialize($products, 'json');
 
         return new Response($data, 200, [
             'Content-type' => 'application/json'
