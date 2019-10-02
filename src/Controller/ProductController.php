63939650074114504b39b2fd4fa7184a17fdd070
Seb Avenel <sebastien.avenel@outlook.fr>
2019-10-02 17:18:52 +0200

    pagination

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -15,14 +16,19 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends Controller
 {
     /**
-     * @Route("/products", methods={"GET"})
+     * @Route("/products/{page<\d+>?1}", methods={"GET"})
+     * @param Request $request
      * @param ProductRepository $repository
      * @param SerializerInterface $serializer
      * @return Response
      */
-    public function index(ProductRepository $repository, SerializerInterface $serializer)
+    public function index(Request $request, ProductRepository $repository, SerializerInterface $serializer)
     {
-        $products = $repository->findAll();
+        $page = $request->query->get('page');
+        if(is_null($page) || $page < 1) {
+            $page = 1;
+        }
+        $products = $repository->findProducts($page, $_ENV['LIMIT']);
         $data = $serializer->serialize($products, 'json');
 
         return new Response($data, 200, [
